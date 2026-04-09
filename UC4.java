193de629b4158913543c9b7667271fe7f2787474
subratyadav-pro <subratyadav92@gmail.com>
2026-04-09 22:31:46 +0530

    added Uc4

---
+++ UC4.java
@@ -0,0 +1,16 @@
+public class UC4 {
+    public static void main(String[] args) {
+
+        String message;
+
+        if (args.length > 0) {
+            // Join all names with comma
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        System.out.println(message);
+    }
+}
